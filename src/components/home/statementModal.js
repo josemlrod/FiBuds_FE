12957e9ec1f047dc7bb2f8d94195e18112ccee72
J <mrjosemlrod@gmail.com>
2019-07-09 10:04:35 -0400

    working on modals styling

--- src/components/home/statementModal.js
+++ src/components/home/statementModal.js
@@ -2,17 +2,53 @@ import React from 'react';
 import {Button, Modal, ModalHeader, ModalBody, ModalFooter,} from 'reactstrap';
 
 export default props => {
+    const handleSubmit = _ => {
+        console.log('you clicked baby')
+    }
     return (
       <div>
-        <Modal isOpen={props.modal} toggle={props.toggle} className=''>
-          <ModalHeader toggle={props.toggle}>Modal title</ModalHeader>
-          <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <Modal isOpen={props.modal} toggle={props.toggle}>
+          {/* <ModalHeader toggle={props.toggle}>Add Statement</ModalHeader> */}
+          <ModalBody className='n-backg-color rounded'>
+            <div className='row' style={{justifyContent: 'center'}}>
+                <div className='col-10 bottom-border-w text-center'>
+                    <h3 className='l-color app-font'>Add Statement</h3>
+                </div>
+            </div>
+
+            <div className='row mt-3' style={{justifyContent: 'center'}}>
+                <div className='col-11 app-font l-color landing-font'>
+                    Name:
+                </div>
+            </div>
+            <div className='row' style={{justifyContent: 'center'}}>
+                <div className='col-11 app-font text-center l-color landing-font'>
+                    <input type='text' className='rounded' style={{width: 'inherit'}} />
+                </div>
+            </div>
+
+            <div className='row mt-2' style={{justifyContent: 'center'}}>
+                <div className='col-11 app-font l-color landing-font'>
+                    Budget:
+                </div>
+            </div>
+            <div className='row' style={{justifyContent: 'center'}}>
+                <div className='col-11 app-font text-center l-color landing-font'>
+                    <input type='text' className='rounded' style={{width: 'inherit'}} />
+                </div>
+            </div>
+
+            <div className='row mt-4 mb-2' style={{justifyContent: 'center'}}>
+                <div className='col-11 text-right'>
+                <button type="submit" className="btn b-backg-color l-color text-center"
+                                        onClick={handleSubmit}>Submit</button>
+                </div>
+            </div>
           </ModalBody>
-          <ModalFooter>
+          {/* <ModalFooter>
             <Button color="primary" onClick={props.toggle}>Do Something</Button>{' '}
             <Button color="secondary" onClick={props.toggle}>Cancel</Button>
-          </ModalFooter>
+          </ModalFooter> */}
         </Modal>
       </div>
     );
