48c9924535e9de8b479002768fee406b1c7bffab
J <mrjosemlrod@gmail.com>
2019-07-23 01:13:21 -0400

    navbar v2 done, need to add other links on to it, will do once components are done

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -74,14 +74,31 @@ export default props => {
                     </ul>
                     
                     <nav>
-                        <div className="nav-wrapper">
-                        <a href="#" className="brand-logo">Logo</a>
-                        <ul id="nav-mobile" className="right hide-on-med-and-down">
-                            <li><a href="sass.html">Sass</a></li>
-                            <li><a href="badges.html">Components</a></li>
-                            <li><a href="collapsible.html">JavaScript</a></li>
-                        </ul>
-                        <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                        <div className="nav-wrapper n-backg-color">
+                            {/* <a href="#" className="border">
+                                <img src={Logo} alt='app-logo'
+                                    style={{height: '-webkit-fill-available'}} />
+                            </a>
+                            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                                <li><a href="sass.html">Sass</a></li>
+                                <li><a href="badges.html">Components</a></li>
+                                <li><a href="collapsible.html">JavaScript</a></li>
+                            </ul>
+                            <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a> */}
+                            <div className='row'>
+                                <div className='col-2'>
+                                    <a href="#" data-target="slide-out" className="sidenav-trigger" style={{textDecoration: 'none'}}><i className="material-icons">menu</i></a>
+                                </div>
+                                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                                    <li><a href="sass.html">Sass</a></li>
+                                    <li><a href="badges.html">Components</a></li>
+                                    <li><a href="collapsible.html">JavaScript</a></li>
+                                </ul>
+                                <div className='col-8'>
+                                    <img src={Logo} alt='app-logo'
+                                        style={{height: '-webkit-fill-available'}} className='brand-logo' />
+                                </div>
+                            </div>
                         </div>
                     </nav>
                 </>
