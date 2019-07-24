04a8a67a1c5e6846a88ddcab3ebf74c5d5a1098c
J <mrjosemlrod@gmail.com>
2019-07-24 00:43:59 -0400

    changed api address back to local server

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const createUser = (
     fName, lName, email, firebase_token, avatar_url, income
 ) => axios({
     method: 'post',
-    url: 'https://fibuds.herokuapp.com/user/',
+    url: 'http://localhost:11235/user/',
     data: {
         first_name: fName,
         last_name: lName,
@@ -17,7 +17,7 @@ const createUser = (
 
 const createStatement = (name, budget, user_id, saved) => axios({
     method: 'post',
-    url: 'https://fibuds.herokuapp.com/statement/',
+    url: 'http://localhost:11235/statement/',
     data: {
         name,
         budget,
@@ -29,12 +29,12 @@ const createStatement = (name, budget, user_id, saved) => axios({
 const getUserByEmail = async email => {
     const userDataCall = await axios({
         method: 'get',
-        url: `https://fibuds.herokuapp.com/user/email/${email}`,
+        url: `http://localhost:11235/user/email/${email}`,
     });
 
     const userStatementCall = await axios({
         method: 'get',
-        url: `https://fibuds.herokuapp.com/statement/all/${email}`,
+        url: `http://localhost:11235/statement/all/${email}`,
     });
 
     const {data: userData,} = userDataCall.data;
@@ -61,7 +61,7 @@ const updateUser = (
     fName, lName, email, firebase_token, avatar_url, income, id
 ) => axios({
     method: 'put',
-    url: 'https://fibuds.herokuapp.com/user/',
+    url: 'http://localhost:11235/user/',
     data: {
         first_name: fName,
         last_name: lName,
